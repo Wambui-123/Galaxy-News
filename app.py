53d8e2a96308024f8b5385a91dca2b23ed952979
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-04-30 21:33:09 +0300

    linking api information to file

--- app.py
+++ app.py
@@ -1,18 +1,41 @@
-from flask import Flask
+from flask import Flask, render_template
+from newsapi import NewsApiClient
 app= Flask(__name__)
-@app.route('/', methods=['GET'])
-def index():
-  return '''
-  <h1>Adopt a Pet!</h1>
-  <p>Browse through the links below to find your new furry friend:</p>
-  <ul>
-  <a href ='/animals/dogs'><li>Dogs</li></a>
-  <a href = '/animals/cats'><li>Cats</li></a>
-  <a href='/animals/rabbits'><li>Rabbits</li></a>
-  </ul>
-  '''
-# @app.route('/animals/<pet_type>')  
-# def  animals(pet_type):
-#   html = f'<h1>List of {pet_type}</h1>'
-#   for 
-#   return html
+@app.route('/')
+def home():
+  # enter api key and client key for authorization
+  newsapi= NewsApiClient(api_key="e9ac9316fc2c41eeab50e67e45e97a4b")
+  
+  # top highlights
+  top_headlines = newsapi.get_top_headlines(sources="abc-news")
+  # Fetch all the top headlights
+  t_articles = top_headlines['articles']
+  #Things I want stored
+  news = []
+  author = []
+  desc = []
+  img = []
+  p_date = []
+  url = []
+  content = []
+  #fetch content from all articles using for loop
+  for i in range(len(t_articles)):
+    main_article = t_articles[i]
+    
+    # Append contents to list 
+    news.append(main_article["title"])
+    author.append(main_article["author"])
+    desc.append(main_article["description"])
+    img.append(main_article["urlToImage"])
+    p_date.append(main_article["publishedAt"])
+    url.append(main_article["url"])
+    content.append(main_article["content"])
+  #merge them in a zip
+  contents = zip(news,author,desc,img,p_date,url,content)
+  #pass it in rendered file
+  return render_template('home.html', contents=contents)
+
+
+
+if __name__ == '__main__':
+  app.run(debug=True)
