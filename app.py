7051979dd6e875d9d8a479e40cac07cbd7f9bc56
Wambui-123 <yvonne.muthui@student.moringaschool.com>
2022-05-03 14:22:39 +0300

    creating new template

--- app.py
+++ app.py
@@ -4,18 +4,16 @@ import datetime
 
 app= Flask(__name__)
 @app.route('/')
-  
+
 def home():
+  newsapi= NewsApiClient(api_key="e9ac9316fc2c41eeab50e67e45e97a4b")  
   # enter api key and client key for authorization
-  newsapi= NewsApiClient(api_key="e9ac9316fc2c41eeab50e67e45e97a4b")
     # top highlights
   top_headlines = newsapi.get_top_headlines(sources="bbc-news")
-    # all articles
-  all_articles = newsapi.get_everything(sources="bbc-news")
+  
     # Fetch all the top headlights
   t_articles = top_headlines['articles']
-  #Fetch all articles  
-  a_articles = all_articles['articles']
+
   #Things I want stored
   news = []
   author = []
@@ -36,7 +34,20 @@ def home():
     url.append(main_article["url"])
     content.append(main_article["content"])
     
+    # Merge
+  contents = zip(news,author,desc,img,p_date,url,content)
+  return render_template('home.html', contents=contents) 
+
+    
   # Create list for all articles
+@app.route('/news')  
+def news():
+  newsapi= NewsApiClient(api_key="e9ac9316fc2c41eeab50e67e45e97a4b")  
+  # all articles
+  all_articles = newsapi.get_everything(sources="bbc-news")
+  #Fetch all articles  
+  a_articles = all_articles['articles']  
+  
   news_all = []
   author_all = []
   desc_all = []
@@ -47,20 +58,18 @@ def home():
 # For loop
   for j in range(len(a_articles)): 
     a_article = a_articles[j]
-    
-#Append the articles into the list
-  news_all.append(a_article["title"])
-  author_all.append(a_article["author"])
-  desc_all.append(a_article["description"])
-  img_all.append(a_article["urlToImage"])
-  p_date_all.append(a_article["publishedAt"])
-  url_all.append(a_article["url"])
-  content_all.append(a_article["content"])     
+  #Append the articles into the list
+    news_all.append(a_article["title"])
+    author_all.append(a_article["author"])
+    desc_all.append(a_article["description"])
+    img_all.append(a_article["urlToImage"])
+    p_date_all.append(a_article["publishedAt"])
+    url_all.append(a_article["url"])
+    content_all.append(a_article["content"])     
  
   # Merge  
-  contents = zip(news,author,desc,img,p_date,url,content)
   all = zip(news_all,author_all,desc_all,img_all,p_date_all,url_all,content_all)
-  return render_template('home.html', contents=contents, all=all) 
+  return render_template('news.html', all=all) 
   
   
 if __name__ == '__main__':
